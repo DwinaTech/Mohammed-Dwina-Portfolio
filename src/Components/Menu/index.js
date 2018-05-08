aae6b510a8b4eb5a4dba8b0c412c71386e5aa0eb
MrDwina <diwina1993@hotmail.com>
2018-05-08 12:59:20 +0100

    edit dwinatech directory stercture

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -72,7 +72,7 @@ class Menu extends React.Component {
           </IconButton>
         </div>
         <Divider />
-        <List onClick={this.handleSideCloser}><Link className={`${!linkName.includes('home') ? "active" : ""}`} to="/">Home</Link></List>
+        <List onClick={this.handleSideCloser}><Link className={`${linkName.includes('/') ? "active" : ""}`} to="/">Home</Link></List>
         <Divider />
         <List onClick={this.handleSideCloser}><Link className={`${linkName.includes('contact') ? "active" : ""}`} to="/contact">Contact</Link></List>
         <Divider />
@@ -107,7 +107,7 @@ class Menu extends React.Component {
             </Hidden>
             <Hidden smDown>
               <div className="list">
-                <li><Link className={`${!linkName.includes('Home') ? "active" : "" }`} to="/">Home</Link></li>
+                <li><Link className={`${linkName.includes('/') ? "active" : "" }`} to="/">Home</Link></li>
                 <li><Link className={`${linkName.includes('contact') ? "active" : ""}`} to="/contact">Contact</Link></li>
                 <li><Link className={`${linkName.includes('about') ? "active" : ""}`} to="/about">About</Link></li>
               </div>
