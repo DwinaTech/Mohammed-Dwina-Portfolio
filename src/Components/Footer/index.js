192a72bba1055cc19a8e90cccca7b85e5c7c22ba
dwinatech <diwina1993@hotmail.com>
2019-07-27 19:51:19 +0100

    Update the footer icons

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './footer.scss';
+import facebook from '../../../public/images/facebook.png';
+import twitter from '../../../public/images/twitter.png';
+import linkedIn from '../../../public/images/linkedIn.png';
 
 const Footer = () => (
   <footer className="footer">
     <div className="container social">
       <Link to="https://twitter.com/diwna201" target="blanket">
-        <i className="fa fa-twitter" />
+        <img src={twitter} alt="twitter" />
       </Link>
       <Link to="https://www.facebook.com/DwinaTech/" target="blanket">
-        <i className="fa fa-facebook" />
+        <img src={facebook} alt="facebook" />
       </Link>
       <Link to="https://www.linkedin.com/in/mohammed-dwina-3842b1141/" target="blanket">
-        <i className="fa fa-linkedin" />
+        <img src={linkedIn} alt="linkedIn" />
       </Link>
     </div>
     <span className="copyright">Copyright &copy; DwinaTech 2018</span>
