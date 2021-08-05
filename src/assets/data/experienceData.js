85220194c1b01b2126f70b3f43d9d2ba7d0de039
Mohammed Dwina <diwina1993@hotmail.com>
2021-08-05 11:19:07 +0100

    Update experience data

--- src/assets/data/experienceData.js
+++ src/assets/data/experienceData.js
@@ -1,11 +1,26 @@
 const data = [
   {
     id: 1,
+    company: "Arnold Clark",
+    role: "Frontend Developer",
+    date: " June 2021 - ongoing",
+    info: `
+    I am working as a Frontend Developer in Arnold Clark digital team. 
+    My main responsibility to help improving/solving Arnold Clark customer buying/renting a car process through the digital system, help shaping tickets, help in the sprint planning, review code, build frontend pipeline, 
+    writing unit tests, continuous refactoring, working with other developers in the team. 
+    The technologies I am using React.js, JavaScript (ES6 syntax), Typescript, CSS3, HTML5.
+    The tools I am using are Eslint, babel,  prettier and Chrome Dev.
+    The version control system I am using is Git and Github.
+    Testing library I use is react-testing-library, jest and storybook.
+    `,
+  },
+  {
+    id: 2,
     company: "BBC D&E",
     role: "Software Engineer",
-    date: " Fab 2020 - ongoing",
+    date: " Fab 2020 - June 2021",
     info: `
-    I am working as software engineer in BBC Home team. 
+    I am working as software engineer in the BBC Home team. 
     My main responsibility to deliver new features, handling ATI tracking functionalities, writing unit tests, continuous refactoring, working with other developers in the team and fixing any bug reported by Opps team. 
     The App we are building is the main BBC Homepage website and other nations pages, it is a data driven web application,I am working in both site front-end and backend.
     We are using React.js, JavaScript (ES6 syntax), CSS3, HTML5, emotion.js, Node.js technologies.
@@ -14,7 +29,7 @@ const data = [
     `,
   },
   {
-    id: 2,
+    id: 3,
     company: "STV Group",
     role: "Front-End Web Developer",
     date: " Aug 2018 - ongoing",
@@ -27,7 +42,7 @@ const data = [
     `,
   },
   {
-    id: 3,
+    id: 4,
     company: "Directory Of Services",
     role: "Junior Full Stack Developer (remote)",
     date: " Jan 2018 - Aug 2018",
@@ -43,7 +58,7 @@ const data = [
     `,
   },
   {
-    id: 4,
+    id: 5,
     company: "J.P. Morgan",
     role: "Code for Good Hackathon",
     date: "Nov 2017, Glasgow",
@@ -52,7 +67,7 @@ const data = [
     `,
   },
   {
-    id: 5,
+    id: 6,
     company: "CodeYourFuture",
     role: "Trainee Full Stack Developer",
     date: "May 2017 - Dec 2017, Glasgow",
