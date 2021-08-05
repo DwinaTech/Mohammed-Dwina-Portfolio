b19c0d7057c24b42772a92dffb434fa0eeb50677
Mohammed Dwina <diwina1993@hotmail.com>
2021-08-05 12:11:18 +0100

    Add missing component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Components/Routes";
-import "./App.scss";
 
 const App = () => (
   <div className="App">
---
+++ src/Components/Spinner/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./spinner.scss";
+
+function Spinner() {
+  return (
+    <div className="spinner">
+      <div className="circle">
+        <label>Loding...</label>
+        <div className="circle-child" />
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
